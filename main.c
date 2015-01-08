dcff15b59ea5ece057c2e49e93fc552b9c581b28
unknown <dupre.thomas.m@gmail.com>
2015-01-08 20:26:34 +0100

    First project working, first window with CSFML

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <SFML/System.h>
+#include <SFML/Window.h>
+#include <SFML/Graphics.h>
 
+#define W_WIDTH 300
+#define W_HEIGHT 300
 int main(){
+    sfVideoMode mode = {W_WIDTH, W_HEIGHT, 32};
+    sfRenderWindow* window;
+    sfEvent event;
+    window = sfRenderWindow_create(mode, "Peter The Beaver",   sfClose, NULL);
 
-    printf("Hello the beaver\n");
-
+    while (sfRenderWindow_isOpen(window))
+    {
+       // Process events
+        while (sfRenderWindow_pollEvent(window, &event))
+        {
+             // Close window : exit
+            if (event.type == sfEvtClosed)
+                sfRenderWindow_close(window);
+        }
+         // Clear the screen
+        sfRenderWindow_clear(window, sfWhite);
+          // Update the window
+        sfRenderWindow_display(window);
+    }
+    sfRenderWindow_destroy(window);
     return 0;
 }
