b7eabeb0816edc3ecd0480770c695dc58802b9fc
unknown <dupre.thomas.m@gmail.com>
2015-01-08 22:25:17 +0100

    Organize folders and files

---
+++ Peter The Beaver Editor/main.c
@@ -0,0 +1,30 @@
+#include <SFML/System.h>
+#include <SFML/Window.h>
+#include <SFML/Graphics.h>
+
+#define W_WIDTH 300
+#define W_HEIGHT 300
+int main(){
+    sfVideoMode mode = {W_WIDTH, W_HEIGHT, 32};
+    sfRenderWindow* window;
+    sfEvent event;
+    window = sfRenderWindow_create(mode, "Peter The Beaver Editor",   sfClose, NULL);
+
+    while (sfRenderWindow_isOpen(window))
+    {
+       // Process events
+        while (sfRenderWindow_pollEvent(window, &event))
+        {
+             // Close window : exit
+            if (event.type == sfEvtClosed)
+                sfRenderWindow_close(window);
+        }
+         // Clear the screen
+        sfRenderWindow_clear(window, sfWhite);
+          // Update the window
+        sfRenderWindow_display(window);
+    }
+    sfRenderWindow_destroy(window);
+    return 0;
+}
+
---
+++ Peter The Beaver/README.md
@@ -0,0 +1,2 @@
+# Peter The Beaver
+C project in the school of engineering Polytech of the university Paris-Sud
